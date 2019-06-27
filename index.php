ed3a49d6f78468babffd383fa6c3be6f8e7b8150
Trần Duy Bá <tranduyba2599@gmail.com>
2019-06-27 15:39:21 +0700

    Back up file 3:39pm-27/6/2019

---
+++ index.php
@@ -0,0 +1,48 @@
+<!-- Chợ HaUi-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chợ HaUi</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="views/css/index.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="head_web">
+            <div id="logo_website">
+
+            </div>
+            <div id="cac_cong_cu">
+                <div class="cong_cu">
+                    <input id="input_tim_kiem" type="text" name="noi_dung_tim_kiem" value="" />
+                    <input id="button_tim_kiem" type="button" value="Tìm kiếm" />
+                </div>
+                <div class="cong_cu">
+                    <p>Sản phẩm</p>
+                </div>
+                <div class="cong_cu">
+                    <p>Thông báo</p>
+                </div>
+                <div class="cong_cu">
+                    <p>Hỗ trợ</p>
+                </div>
+                <div class="cong_cu">
+                    <input type="button" value="Đăng nhập" />
+                </div>
+            </div>
+        </div>
+        <div id="body_web">
+            <div id="cong_cu_loc">
+                <div>
+                    <p>Mới nhất</p>
+                </div>
+                <div>
+                    <p>Giá tăng dần</p>
+                </div>
+                <div>
+                    <p>Giá giảm dần</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
