1f663481bf693d93348cd5c93420a732cb050016
Trần Duy Bá <tranduyba2599@gmail.com>
2019-07-01 14:41:20 +0700

    Back up Front End 2:41pm-1/7/2019

---
+++ My_blog/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BL Blog</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="views/css/header.css" />
+        <link rel="stylesheet" type="text/css" href="views/css/content_index.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="header">
+            <div id="logo">
+                <a href="#" title="BL Blog">
+                    <img src="views/images/img_designs/My_Logo.png" />
+                    <h1>BL-Bá Linh</h1>
+                </a>
+            </div>
+            <div id="slogan">
+                <p>Phương châm sống !</p>
+            </div>
+            <div id="info">
+                <a href="">
+                    <img src="views/images/img_designs/facebook.png" title="facebook" />
+                </a>
+                <a href="">
+                    <img src="views/images/img_designs/github.png" title="GitHub" />
+                </a>
+                <a href="">
+                    <img src="views/images/img_designs/gmail.png" title="Gmail" /> 
+                </a>
+            </div>
+        </div>
+        <div id="content">
+            <div class="posts">
+                <h2>
+                    <a href="">Tìm việc như thế nào? Developer tìm việc như thế nào??</a>
+                </h2>
+                <p>Một câu hỏi thường gặp của những bạn sinh viên mới ra trường Một câu hỏi thường gặp của những bạn ng bạn sinh viên mới ra trường, thậm chí là những bạn junior developer dưới một năm kinh nghiệm đó là: Developer tìm việc như thế nào?  Mình đã từng trả lời rất nhiều câu hỏi dạng như vậy nên hôm nay mình sẽ chia sẻ một số sinh viên mới ra trường, thậm chí là những bạn junior developer dưới một năm kinh nghiệm đó là: Developer tìm việc như thế nào?  Mình đã từng trả lời rất nhiều câu hỏi dạng như vậy nên hôm nay mình sẽ chia sẻ một số kin </p>
+                <input type="button" value="Xem bài viết..." />
+                <div>
+                    <img src="views/images/img_designs/writer.png" title="Người viết" /> <p>Trần Duy Bá</p>
+                    <img src="views/images/img_designs/views.png" title="Số lượt xem" /> <p>20</p>
+                    <img src="views/images/img_designs/comment.png" title="Số bình luận" /> <p>30</p>
+                    <img src="views/images/img_designs/share.png" title="Chia sẻ" /> <p>30</p>
+                    <img src="views/images/img_designs/list.png" title="Danh mục" /> <p>Chuyện thường ngày Chuyện thường ngày </p>
+                </div>
+            </div>
+            <div class="posts">
+                <h2>
+                    <a href="">Tìm việc như thế nào? Developer tìm việc như thế nào?? việc như thế nào? Developer tìm việc như thế nào??</a>
+                </h2>
+                <p>Một câu hỏi thường gặp của những bạn sinh viên mới ra trường Một câu hỏi thường gặp của những bạn ng bạn sinh viên mới ra trường, thậm chí là những bạn junior developer dưới một năm kinh nghiệm đó là: Developer tìm việc như thế nào?  Mình đã từng trả lời rất nhiều câu hỏi dạng như vậy nên hôm nay mình sẽ chia sẻ một số sinh viên mới ra trường, thậm chí là những bạn junior developer dưới một năm kinh nghiệm đó là: Developer tìm việc như thế nào?  Mình đã từng trả lời rất nhiều câu hỏi dạng như vậy nên hôm nay mình sẽ chia sẻ một số kin </p>
+                <input type="button" value="Xem bài viết..." />
+                <div>
+                    <img src="views/images/img_designs/writer.png" title="Người viết" /> <p>Trần Duy Bá</p>
+                    <img src="views/images/img_designs/views.png" title="Số lượt xem" /> <p>20</p>
+                    <img src="views/images/img_designs/comment.png" title="Số bình luận" /> <p>30</p>
+                    <img src="views/images/img_designs/share.png" title="Chia sẻ" /> <p>30</p>
+                    <img src="views/images/img_designs/list.png" title="Danh mục" /> <p>Chuyện thường ngày Chuyện thường ngày </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
