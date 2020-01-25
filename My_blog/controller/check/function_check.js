795b26135dd332fc146c0a5f89281d7863bd409b
batran2599 <tranduyba2599@gmail.com>
2020-01-25 14:35:44 +0700

    Delete folder My_blog

--- My_blog/controller/check/function_check.js
+++
@@ -1,9 +0,0 @@
-function repeat_passoword(pass_new,repeat_pass_new){
-    var p1 = document.getElementById(pass_new).value;
-    var p2 = document.getElementById(repeat_pass_new).value;
-    if(p1!=p2){
-        alert('Mật khẩu mới không trùng khớp !');
-        return false;
-    } else 
-        return true;
-}
